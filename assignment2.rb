ce317ed7b39e1ac0e996800264606c4463e4767b
andrewjbotros <andrewjbotros@gmail.com>
2014-01-14 20:56:22 -0500

    omg, progress. Hollar at me

--- assignment2.rb
+++ assignment2.rb
@@ -6,15 +6,37 @@
 
 #The rounding rules for sales tax are that for a tax rate of n%, a shelf price of p contains (np/100 rounded up to the nearest 0.05) amount of sales tax.
 #Write an application that prints out the receipt details for these shopping baskets...
-items = {}
 
-items = {
-  :book => {"exempt", 12.49},
-  :cohort2 => 42,
-  :cohort3 => 22
-}
-class SalesTaxes
+#create 3 baskets of goodies
+basket1 = {}
+basket2 = {}
+basket3 = {}
 
+basket1["book"] = ["exempt", 12.49]
+basket1["music CD"] = ["sales", 14.99]
+basket1["chocolate bar"] = ["exempt", 0.85]
+items["imported box of chocolates"] = ["import", 10.00]
+items["imported bottle of perfume"] = ["import", 47.50]
+items["perfume2"] = ["import", 27.99]
+items["perfume3"] = ["sales", 18.99]
+items["headachePills"] = ["exempt", 9.75]
+items["chocolates2"] = ["import", 11.25]
 
+#assign 3 baskets with goods
+basket1 = [items[:book], items[:musicCD], items[:chocolateBar]]
+basket2 = [items[:chocolates], items[:perfume]]
+basket3 = [items[:perfume2], items[:perfume3], items[:headachePills], items[:chocolates2]]
+#check to see what happens
+# puts basket1
+# puts basket2
+# puts basket3
 
+def display (basket)
+	basket.each do |key, value|
+    puts "#{key}: #{value}"
+	end
 end
+
+display(basket1)
+#puts items[:book]
+
