6cecd301cbcf0ac3330233a8df5cef5a6a2bb2c1
andrewjbotros <andrewjbotros@gmail.com>
2014-02-01 16:40:49 -0500

    got it working

--- mars_rover.rb
+++ mars_rover.rb
@@ -37,63 +37,77 @@ $coordinates["y"] = 5
 
 $rover1["x"] = 1
 $rover1["y"] = 2
-$rover1["direction"] = ["N"]
-$rover1["instructions"] = ["LMLMLMLMM"]
+$rover1["direction"] = "N"
+$rover1["instructions"] = "LMLMLMLMM"
 
 $rover2["x"] = 3
 $rover2["y"] = 3
-$rover2["direction"] = ["E"]
-$rover2["instructions"] = ["MMRMMRMRRM"]
+$rover2["direction"] = "E"
+$rover2["instructions"] = "MMRMMRMRRM"
 
 class Rover
 	attr_accessor :x, :y, :direction, :instructions
 
 	def initialize (x, y, direction, instructions)
-		@x = x
-		@y = y
+		@x = x.to_i
+		@y = y.to_i
 		@direction = direction
 		@instructions = instructions
 		@compass = ["N", "E", "S", "W"]
 	end
 
-	def turn (path, direction)
-		if path = "L"
+	def move (path, direction)
+		if path == "L"
 			if direction == "N"
-				direction = "W"
+				@direction = "W"
 			elsif direction == "E"
-				direction = "N"
+				@direction = "N"
 			elsif direction == "S"
-				direction = "E"
+				@direction = "E"
 			elsif direction == "W"
-				direction = "S"
+				@direction = "S"
 			end
 
-		elsif path = "R"
+		elsif path == "R"
 			if direction == "N"
-				direction = "E"
+				@direction = "E"
 			elsif direction == "E"
-				direction = "S"
+				@direction = "S"
 			elsif direction == "S"
-				direction = "W"
+				@direction = "W"
 			elsif direction == "W"
-				direction = "N"
+				@direction = "N"
+			end
+
+		elsif path == "M"
+			if @direction == "N"
+				@y = @y + 1
+			elsif @direction == "E"
+				@x = @x + 1
+			elsif @direction == "S"
+				@y = @y - 1
+			elsif @direction == "W"
+				@x = @x - 1
 			end
 		end
-		@direction = direction
 	end
 
-	def move (direction)
-		if direction = "N"
-			y += 1
-		elsif direction = "E"
-			x += 1
-		elsif direction = "S"
-			y -= 1
-		elsif direction = "W"
-			x -= 1
+	def giggity
+		while @instructions.length > 0
+			path = @instructions.slice!(0)
+			move(path,@direction)
 		end
 	end
 end
 
-Rover1 = Rover.new($rover1["x"], $rover2["y"], $rover1["direction"], $rover1["instructions"])
+rover1 = Rover.new($rover1["x"], $rover1["y"], $rover1["direction"], $rover1["instructions"])
+rover1.giggity
+puts rover1.x
+puts rover1.y
+puts rover1.direction
 
+rover2 = Rover.new($rover2["x"], $rover2["y"], $rover2["direction"], $rover2["instructions"])
+rover2.giggity
+puts rover2.x
+puts rover2.y
+puts rover2.direction
