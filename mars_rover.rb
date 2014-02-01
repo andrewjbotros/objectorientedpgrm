2f8f0db8f9e31809c6a592092efe37207bfe7650
andrewjbotros <andrewjbotros@gmail.com>
2014-02-01 15:51:24 -0500

    update rover

--- mars_rover.rb
+++ mars_rover.rb
@@ -27,66 +27,73 @@
 # 5 1 E
 
 #create required hashes
-@coordinates = {}
-rover1 = {}
-rover2 = {}
+$coordinates = {}
+$rover1 = {}
+$rover2 = {}
 
 #populate these hashes with the appropriate information
-@coordinates["x"] = 5
-@coordinates["y"] = 5
+$coordinates["x"] = 5
+$coordinates["y"] = 5
 
-rover1["x"] = 1
-rover1["y"] = 2
-rover1["direction"] = ["N"]
-rover1["path"] = ["LMLMLMLMM"]
+$rover1["x"] = 1
+$rover1["y"] = 2
+$rover1["direction"] = ["N"]
+$rover1["instructions"] = ["LMLMLMLMM"]
 
-rover2["x"] = 3
-rover2["y"] = 3
-rover2["direction"] = ["E"]
-rover2["path"] = ["MMRMMRMRRM"]
+$rover2["x"] = 3
+$rover2["y"] = 3
+$rover2["direction"] = ["E"]
+$rover2["instructions"] = ["MMRMMRMRRM"]
 
+class Rover
+	attr_accessor :x, :y, :direction, :instructions
 
-def turn (direction)
-	if direction == "L"
-		puts "left"
-	elsif direction == "R"
-		puts "right"
-	else
-		return
+	def initialize (x, y, direction, instructions)
+		@x = x
+		@y = y
+		@direction = direction
+		@instructions = instructions
+		@compass = ["N", "E", "S", "W"]
+	end
+
+	def turn (path, direction)
+		if path = "L"
+			if direction == "N"
+				direction = "W"
+			elsif direction == "E"
+				direction = "N"
+			elsif direction == "S"
+				direction = "E"
+			elsif direction == "W"
+				direction = "S"
+			end
+
+		elsif path = "R"
+			if direction == "N"
+				direction = "E"
+			elsif direction == "E"
+				direction = "S"
+			elsif direction == "S"
+				direction = "W"
+			elsif direction == "W"
+				direction = "N"
+			end
+		end
+		@direction = direction
+	end
+
+	def move (direction)
+		if direction = "N"
+			y += 1
+		elsif direction = "E"
+			x += 1
+		elsif direction = "S"
+			y -= 1
+		elsif direction = "W"
+			x -= 1
+		end
 	end
 end
 
-
-
-
-
-
-
-
-
-#let's visualize an initial map of where everything is
-@grid_row = []
-(0...@coordinates["x"]).each do |i|
-        @grid_row.push("O")
-end
-
-@grid = []
-(0...@coordinates["y"]).each do |j|
-        @grid.push(@grid_row)
-end
-
-def print_grid
-        (0...@coordinates["y"]).each do |value|
-                print @grid[value]
-                puts ""
-        end
-end
-
-#set the rovers in their initial positions
-@grid[rover1["x"] - 1][rover1["y"] - 1] = "R1"
-@grid[rover2["x"] - 1][rover2["y"] - 1] = "R2"
-print_grid
-
-
-
+Rover1 = Rover.new($rover1["x"], $rover2["y"], $rover1["direction"], $rover1["instructions"])
 
